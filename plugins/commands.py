b6858d17b796b0ab126d2e947b851ccf0b5d20e7
Raj Verma <146803011+Sona779988@users.noreply.github.com>
2024-07-09 13:31:11 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -238,6 +238,9 @@ async def start(client:Client, message):
                                    InlineKeyboardButton('❍ ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ ❍', url='https://t.me/SB_Botz_Update'),
                                    
                                  ]]
+                        loading_sticker_message = await message.reply_sticker("CAACAgIAAxkBAALmzGXSSt3ppnOsSl_spnAP8wHC26jpAAJEGQACCOHZSVKp6_XqghKoHgQ"),
+                        await asyncio.sleep(2)
+                        await loading_sticker_message.delete()
                         reply_markup = InlineKeyboardMarkup(buttons)
                         await message.reply_photo(photo=START_IMG, caption=script.START_TXT.format(message.from_user.mention, get_status(), message.from_user.id),
                             reply_markup=reply_markup,
