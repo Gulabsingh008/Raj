fc9a56475449c7f33b4be873896b7d39079af0e5
Raj Verma <146803011+Sona779988@users.noreply.github.com>
2024-07-19 15:18:11 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -214,7 +214,7 @@ async def start(client:Client, message):
         await client.send_message(LOG_CHANNEL, script.NEW_USER_TXT.format(temp.B_LINK, message.from_user.id, message.from_user.mention))
         try: 
             refData = message.command[1]
-            if refData and refData.split("-", 1)[0] == "Sb_botz_update":
+            if refData and refData.split("-", 1)[0] == "Biisal":
                 Fullref = refData.split("-", 1)
                 refUserId = int(Fullref[1])
                 await db.update_point(refUserId)
