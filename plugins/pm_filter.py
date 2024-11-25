7c7ad2d726223da3a8aa3fe667da114d2ec92883
Raj Verma <146803011+Sona779988@users.noreply.github.com>
2024-11-25 22:51:25 +0530

    Update and rename pm_filter (1).py to pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -850,7 +850,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             ],
             [
 		InlineKeyboardButton('• ᴜʀ ᴘᴏɪɴᴛs •', callback_data=f'point#{query.from_user.id}'), 
-		InlineKeyboardButton('• ᴄᴜsᴛᴏᴍ •', callback_data='custom')
+		InlineKeyboardButton('• ɢʀᴏᴜᴘ ᴀᴅᴍɪɴ ᴄᴍᴅ •', callback_data='custom')
 	    ],
             [
                 InlineKeyboardButton('• ғsᴜʙ •', callback_data='fsub'), 
