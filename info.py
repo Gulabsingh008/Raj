23aec52944ea887fb01349169141283a2090f98d
Gulabsingh008 <gulabsinghrajput008@gmail.com>
2024-12-11 20:53:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ QR_CODE = environ.get('QR_CODE', 'https://graph.org/file/d7c9c7906833797aa0244.j
 START_IMG = environ.get('START_IMG', 'https://graph.org/file/82e2895b4740905b054b9.gif')
 #this vars is for when heroku or koyeb acc get banned, then change this vars as your file to link bot name
 BIN_CHANNEL = int(environ.get('BIN_CHANNEL','-1002030723564'))
-URL = environ.get('URL', 'http://efficient-carlye-myfilestream-c9938a22.koyeb.app')
+URL = environ.get('URL', 'doubtful-octavia-botslearning-960cdd7c.koyeb.app')
 ONEPAGEYAM = 'krownlinks.com'
 STICKERS_IDS = ('CAACAgQAAxkBAAEK99dlfC7LDqnuwtGRkIoacot_dGC4zQACbg8AAuHqsVDaMQeY6CcRojME').split()
 
@@ -65,7 +65,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '-1002235094272'))
 
 # hastags request features
-request_channel = environ.get('REQUEST_CHANNEL', '-1002281015574')
+request_channel = environ.get('REQUEST_CHANNEL', '-1002378415644')
 REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(request_channel) else None
 UPI_PAY_LOGS = int(environ.get('UPI_PAY_LOGS', '-1002030723564'))
 
