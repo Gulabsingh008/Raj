41e295b3675a5c9971084e5fcc5097cf440e5ba9
Gulabsingh008 <gulabsinghrajput008@gmail.com>
2024-12-14 07:40:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,7 +22,7 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 USERNAME = environ.get('USERNAME', "https://telegram.me/Af_mhakal")
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002030723564'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+Xz6Art-q091lODg1')
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002194109031').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002224002358 -1002194109031').split()]
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://fplonq:CXBRdJeEAdtKbV6y@cluster0.q7dfa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://2o8jsj:IHSJ8FC65WChypWS@cluster0.5tmub.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
